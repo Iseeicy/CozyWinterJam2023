876e2030f6609397340244ea455d339d21995742
Iseeicy <iseeicyhawkston@gmail.com>
2023-12-17 06:14:35 -0600

    Updated icicle rule

--- tiles/rules/icicle_rule.gd
+++ tiles/rules/icicle_rule.gd
@@ -14,3 +14,10 @@ func get_tile_id() -> int:
 
 func grapple_locked_tile(_tile_map: TileMap, _position: Vector2i, grapple: Grapple3, _point: Vector2, _normal: Vector2, player: BallsPlayer) -> void:
     player.unthrow_grapple(grapple)
+
+func hit_tile(_tile_map: TileMap, position: Vector2i, body: RigidBody2D, player: BallsPlayer) -> void:
+    if get_is_handling(position): return
+    set_is_handling(position, true)
+    
+    player.kill(body, BallsPlayer.KillType.Shatter)
+    set_is_handling(position, false)
