0b3eaadc5d9449772341015be09645ecb95499c4
Iseeicy <iseeicyhawkston@gmail.com>
2023-12-17 01:02:15 -0600

    Swing test works WAY better

--- move_toward_mouse.gd
+++ move_toward_mouse.gd
@@ -3,4 +3,5 @@ extends RigidBody2D
 @export var move_force: float = 10000
 
 func _physics_process(delta):
-	apply_central_force((get_global_mouse_position() - global_position).normalized() * move_force * delta)
+	if Input.is_action_pressed("shoot_hook_a"):
+		apply_central_force((get_global_mouse_position() - global_position).normalized() * move_force * delta)
