debacc0fe48edd610d7031d202e3b264ba7949fd
Iseeicy <iseeicyhawkston@gmail.com>
2023-12-20 17:33:48 -0600

    Added door and checkpoint audio

--- tiles/checkpoint/checkpoint.gd
+++ tiles/checkpoint/checkpoint.gd
@@ -3,11 +3,18 @@ class_name Checkpoint
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var _is_up: bool = false
+
 func _ready():
 	set_flag_is_up(false)
 
 func set_flag_is_up(is_up: bool):
 	sprite.animation = "up" if is_up else "down"
+
+	if is_up == _is_up: return
+	_is_up = is_up
+
+	if _is_up: $LightUpAudio.play()
 
 func _on_body_entered(_body: Node2D):
 	CheckpointManager.flag_new_checkpoint(self)
--- tiles/doors/doorway.gd
+++ tiles/doors/doorway.gd
@@ -7,6 +7,7 @@ class_name Doorway
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var _is_open: bool = false
 
 func _ready():
 	PresentManager.collected_updated.connect(_on_present_collection_updated.bind())
@@ -16,5 +17,13 @@ func _on_present_collection_updated(held: Dictionary, saved: Dictionary):
 	call_deferred("_set_open", count >= required_currency)
 
 func _set_open(is_open: bool):
+	if _is_open == is_open: return
+	
 	animated_sprite.animation = "open" if is_open else "closed"
 	collider.disabled = is_open
+	_is_open = is_open
+
+	if is_open:
+		$OpenAudio.play()
+	else:
+		$CloseAudio.play()
